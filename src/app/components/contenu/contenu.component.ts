db31ce0b10d4bd7bebc5225ee550ba2d8e54e6e5
Julien MARTIN <ju.martin@outlook.com>
2024-06-14 15:54:51 +0200

    add loader, add mapping of obs and protocoles, add cards

--- src/app/components/contenu/contenu.component.ts
+++ src/app/components/contenu/contenu.component.ts
@@ -6,11 +6,14 @@ import {forkJoin} from "rxjs";
 import {Observation} from "../../models/observation";
 import {Ontologie} from "../../models/ontologie";
 import {Protocole} from "../../models/protocole";
+import {CardComponent} from "../card/card.component";
 
 @Component({
   selector: 'app-contenu',
   standalone: true,
-  imports: [],
+  imports: [
+    CardComponent
+  ],
   templateUrl: './contenu.component.html',
   styleUrl: './contenu.component.css'
 })
@@ -61,7 +64,15 @@ export class ContenuComponent {
     forkJoin([getOntologie, getProtocoles]).subscribe({
       next: (data) => {
         this.pays = data[0]
-        this.protocoles = data[1]["resultats"];
+
+        // On convertir les données reçu au format de notre objet
+        const protocolesData = data[1]["resultats"];
+        if (protocolesData && typeof protocolesData === 'object') {
+          this.protocoles = this.mapProtocoles(Object.values(protocolesData));
+        } else {
+          console.error('Expected object for protocoles data but got:', protocolesData);
+          this.protocoles = [];
+        }
       },
       error: (err) => {
         this.errorMessage = err.error.error
@@ -87,7 +98,15 @@ export class ContenuComponent {
           let token = data.token;
           this.delService.getObservations(this.urlParamsString, token).subscribe( {
             next: (data) => {
-              this.observations = data["resultats"];
+              // On convertir les données reçu au format de notre objet
+              const observationsData = data["resultats"];
+              if (observationsData && typeof observationsData === 'object') {
+                this.observations = this.mapObservations(Object.values(observationsData));
+              } else {
+                console.error('Expected object for protocoles data but got:', observationsData);
+                this.observations = [];
+              }
+
               this.observationsEntete = data["entete"];
               this.nbObservations = this.observationsEntete.total
               this.isLoading = false;
@@ -108,7 +127,15 @@ export class ContenuComponent {
       // Si l'utilisateur n'est pas connecté, on va récupérer les obs sans header authorization
       this.delService.getObservations(this.urlParamsString).subscribe({
         next: (data) => {
-          this.observations = data["resultats"];
+          // On convertir les données reçu au format de notre objet
+          const observationsData = data["resultats"];
+          if (observationsData && typeof observationsData === 'object') {
+            this.observations = this.mapObservations(Object.values(observationsData));
+          } else {
+            console.error('Expected object for protocoles data but got:', observationsData);
+            this.observations = [];
+          }
+
           this.observationsEntete = data["entete"];
           this.nbObservations = this.observationsEntete.total
           this.isLoading = false;
@@ -119,5 +146,44 @@ export class ContenuComponent {
         }
       });
     }
+  }
+
+  private mapProtocoles(data: any[]): Protocole[] {
+    return data.map(item => new Protocole(
+      item["protocole.descriptif"],
+      item["protocole.id"],
+      item["protocole.identifie"],
+      item["protocole.intitule"],
+      item["protocole.mots_cles"],
+      item["protocole.tag"]
+    ));
+  }
+
+  private mapObservations(data: any[]): Observation[]{
+    return data.map(item => new Observation(
+      item["auteur.courriel"],
+      item["auteur.id"],
+      item["auteur.nom"],
+      item["commentaires"],
+      item["date_observation"],
+      item["date_transmission"],
+      item["determination.famille"],
+      item["determination.nn"],
+      item["determination.ns"],
+      item["determination.nt"],
+      item["determination.referentiel"],
+      item["hauteur"],
+      item["id_image"],
+      item["id_observation"],
+      item["id_zone_geo"],
+      item["images"],
+      item["largeur"],
+      item["mots_cles_texte"],
+      item["nb_commentaires"],
+      item["nom_original"],
+      item["pays"],
+      item["station"],
+      item["zone_geo"]
+    ))
   }
 }
