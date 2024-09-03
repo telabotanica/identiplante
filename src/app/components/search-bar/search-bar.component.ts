f8489dd6da9deb97bae2151c4a8f47e03fefb88f
Julien MARTIN <ju.martin@outlook.com>
2024-09-03 14:58:54 +0200

    switch advanced search section from popup to accordion

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -53,7 +53,7 @@ export class SearchBarComponent {
   }
 
   openAdvancedSearchPopUp(){
-    this.displayAdvancedPopup = true
+    this.displayAdvancedPopup = !this.displayAdvancedPopup
   }
 
   closeAdvancedSearchPopUp() {
